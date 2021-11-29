8238b1d42357eaee0a62cfa473ca910d4877c2e5
Alankrit Arya <alankritarya15@gmail.com>
2021-11-29 16:56:32 +0530

    basic project

--- server/api/models.py
+++ server/api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Question(models.Model):
+    title = models.CharField(max_length=255)
+    type = models.IntegerField()
+
+class Option(models.Model):
+    question = models.ForeignKey(Question,on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    marks = models.IntegerField()  
