2916e4cedf4f59dd841998da7b6fcc9cbde20ea9
Alankrit Arya <alankritarya15@gmail.com>
2021-11-29 15:21:46 +0530

    first

---
+++ front/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import TestScreen from "./screens/TestScreen";
+
+function App() {
+  return (
+    <div className="App">
+      <TestScreen/>
+    </div>
+  );
+}
+
+export default App;
---
+++ front/src/screens/TestScreen.js
@@ -0,0 +1,23 @@
+import React,{useState,useEffect} from 'react'
+
+function TestScreen() {
+    const [hard,setHard] = useState([])
+    const [medium,setMedium] = useState([])
+    const [easy,setEasy] = useState([])
+    const [qs,setQs] = useState([])
+    const [ans,setAns] = useState(false)
+
+    useEffect(()=>{
+    },[])
+
+    function click(){
+
+    }
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default TestScreen
